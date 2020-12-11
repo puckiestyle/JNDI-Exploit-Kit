054b79f6269c77bbaa122c067c35e4d67caf9010
pimps <marcioalma@gmail.com>
2020-12-12 00:00:24 +1100

    commiting updates

---
+++ src/test/java/ExecTemplateJDK5.java
@@ -0,0 +1,11 @@
+/**
+ * @Classname ExecTemplateJDK6
+ * @Author Welkin
+ */
+public class ExecTemplateJDK5 {
+
+    static {
+        System.out.println();
+    }
+
+}
---
+++ src/test/java/ExecTemplateJDK6.java
@@ -0,0 +1,11 @@
+/**
+ * @Classname ExecTemplateJDK6
+ * @Author Welkin
+ */
+public class ExecTemplateJDK6 {
+
+    static {
+        System.out.println();
+    }
+
+}
